c08b086def88dd264435acb6c214c5f29108cfbc
Xiaocheng <houxcnj@hotmail.com>
2017-11-08 10:30:12 -0800

    revise

--- addTwoNumbers/Solution.java
+++ addTwoNumbers/Solution.java
@@ -1,21 +1,6 @@
-import java.util.HashMap;
-
+package addTwoNumbers;
 
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                result[1] = i;
-                result[0] = map.get(target - nums[i]);
-                return result;
-            }
-            map.put(nums[i], i);
-        }
-        return result;
-    }
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode prev = new ListNode(0);
         ListNode head = prev;
@@ -37,4 +22,3 @@ public class Solution {
     }
 
 }
-
