afadd541917f7d1cc7955b0b8be8e7d46603d89b
Xiaocheng <houxcnj@hotmail.com>
2017-11-08 17:29:18 -0800

    continue

---
+++ TicTacToe/TicTacToe.java
@@ -0,0 +1,26 @@
+package TicTacToe;
+
+class TicTacToe {
+	private int[] rows;
+	private int[] cols;
+	private int dia;
+	private int antiDia;
+
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.TicTacToe
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+        
+    }
+}
