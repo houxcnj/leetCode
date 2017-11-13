9be800025e39b0f6e4104eef717a99bbc488d922
houxcnj <houxcnj@hotmail.com>
2017-11-13 00:14:17 -0800

    minstack

---
+++ minStack/minStack.py
@@ -0,0 +1,40 @@
+class MinStack(object):
+	def __init__(self):
+		"""
+		initialize your data structure here.
+		"""
+		self.stack = []
+
+	def push(self, x):
+		"""
+		:type x: int
+		:rtype: nothing
+		"""
+		curMin = self.getMin()
+		if curMin == None or curMin > x:
+			curMin = x
+
+		self.stack.append((x, curMin))
+
+	def pop(self):
+		"""
+		:rtype: nothing
+		"""
+		self.stack.pop()
+
+	def top(self):
+		"""
+		:rtype: int
+		"""
+		if not self.stack:
+			return None
+		return self.stack[-1][0]
+
+	def getMin(self):
+		"""
+		:rtype: int
+		"""
+		if not self.stack:
+			return None
+		return self.stack[-1][1]
+		
